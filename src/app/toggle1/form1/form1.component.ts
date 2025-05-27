4f966f37dac8e203ffaee7abdc0244a0ceb0f5d5
cesar lopez <cesarlopezortiz902@gmail.com>
2025-05-26 23:37:31 -0600

    Accordion by angular/material, creating a components using the library,

--- src/app/toggle1/form1/form1.component.ts
+++ src/app/toggle1/form1/form1.component.ts
@@ -1,11 +1,21 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-form1',
-  imports: [],
+  imports: [CdkAccordionModule],
   templateUrl: './form1.component.html',
   styleUrl: './form1.component.css'
 })
 export class Form1Component {
-
+  items = ['Item 1', 'Item 2', 'Item 3'];
+  itemVisible=true;
+  seCerro(dato: string){
+    alert(`se cerro el ${dato}`)
+  }
+  seDestruyo(){
+    this.itemVisible=false;
+    // alert('destroy')
+  }
 }
